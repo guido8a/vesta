d4598e275ca09ca986941105d51399756cc24600
guido8a <guido8a@gmail.com>
2015-08-18 13:16:51 -0500

    dataSource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -3,9 +3,11 @@ dataSource {
     driverClassName = "org.postgresql.Driver"
     dialect = org.hibernate.dialect.PostgreSQLDialect
 }
+
+// ver: http://grails.github.io/grails-howtos/en/performanceTuning.html
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
+    cache.use_second_level_cache = false  //true por defecto
+    cache.use_query_cache = false  //true por defecto, pero tarda mucho el hibernate
     cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
@@ -13,9 +15,10 @@ environments {
     development {
         dataSource {
             dbCreate = "update"
-//            url = "jdbc:postgresql://10.0.0.2:5432/vesta_hoy"
-            url = "jdbc:postgresql://10.0.0.2:5432/vesta_prba1"
-//            url = "jdbc:postgresql://10.0.0.2:5432/vesta_prdc"
+            url = "jdbc:postgresql://10.0.0.2:5432/vesta_curso"
+//            url = "jdbc:postgresql://10.0.0.2:5432/vesta"
+//            url = "jdbc:postgresql://10.0.0.2:5432/vesta_prba"
+//            url = "jdbc:postgresql://10.0.0.2:5432/vesta_prdc2"
             username = "postgres"
             password = "postgres"
         }
@@ -41,6 +44,15 @@ environments {
         dataSource {
             dbCreate = "update"
             url = "jdbc:postgresql://127.0.0.1:5432/vesta_prba"
+            username = "postgres"
+            password = "steinsgate"
+        }
+    }
+
+    corrientes {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:postgresql://127.0.0.1:5432/vesta_corr"
             username = "postgres"
             password = "steinsgate"
         }
